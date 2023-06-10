b6cb3c30384b36b63decd4b474d49000f7f09b87
aisha <12617103+asiddiq1@users.noreply.github.com>
2023-06-10 11:58:15 -0700

    Final version of rps game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getComputerChoice(){
+    //randomly selects computer choice
     let choice = Math.floor(Math.random() * 3);
 
     if (choice == 0){
@@ -13,55 +14,142 @@ function getComputerChoice(){
 
 }
 
-function playRound(playerSelection, computerSelection){
-    let playerChoice = playerSelection.toLowerCase();
+function playRound(playerChoice, computerSelection){
+    //plays single round and updates text whether user won or lost
+    let getWinnerNote = document.querySelector('.winner_note');
+    let winnerText = null;
 
     if (playerChoice == computerSelection){
-        return "It's a draw!";
+        winnerText = `It's a draw! ${playerChoice} ties ${computerSelection}.`;
     }
     else if ((playerChoice == 'rock' && computerSelection == 'scissors') ||
              (playerChoice == 'paper' && computerSelection == 'rock') ||
              (playerChoice == 'scissors' && computerSelection == 'paper'))
     {
             
-        return `You win! ${playerChoice} beats ${computerSelection}`;
+        winnerText = `You win! ${playerChoice} beats ${computerSelection}.`;
     } 
     else{
-        return `You lose! ${computerSelection} beats ${playerChoice}`;
+        winnerText = `You lose! ${computerSelection} beats ${playerChoice}.`;
 
     }
+    getWinnerNote.textContent = winnerText; //updates note whether user won or lost
+    return winnerText;
+
 
 }
 
-const playerChoice = 'scissors';
-const computerChoice = getComputerChoice();
-
-console.log(playRound(playerChoice, computerChoice)); 
-
-function game(){
-    let player_count = 0;
-    let computer_count = 0; 
-    for (let i = 0; i < 5; i++){
-        let playerChoice = prompt("Enter a choice (rock/paper/scissors):");
-        let computerChoice = getComputerChoice(); 
-        let getWinner = playRound(playerChoice, computerChoice); 
-        if (getWinner.includes("win")){
-            player_count += 1;
-        }
-        else if (getWinner.includes("lose")){
-            computer_count += 1; 
-        }
+function toggleChoices(computerChoice, playerChoice, count){
+    //toggles highlights choices
+    let removeSelection = Array.from(document.querySelectorAll('ul li i')); 
+    for (const selection of removeSelection){
+        selection.classList.remove('computer-active'); 
 
     }
     
-    if (player_count == computer_count && player_count > 0){
-        return `It's a tie ${player_count} - ${computer_count}`;
-    }
-    else if (player_count < computer_count){
-        return `You lose ${player_count} - ${computer_count}`;
+    let comp = document.querySelector(`.computer-rps .${computerChoice}`);
+    comp.classList.add('computer-active'); //activates robot selection
+
+    let player = document.querySelector(`.player-rps .${playerChoice}`);
+
+    if (count == 4){      
+        player.classList.add('computer-active'); //activates user selection
     }
     else{
-        return `You win ${player_count} - ${computer_count}`;
+        player.classList.toggle('computer-active'); //activates user selection
     }
+   
+}
+
+function playAgain(){
+    window.location.reload();
+} 
+
+const choices = document.querySelector('.player-rps');
+count = 0 
+
+function playGame(){
+
+
+    choices.addEventListener("click", (event) => {
+        if (event.target.nodeName != "I" || choices.disabled){
+            return;
+        }
+
+        let playerChoice = event.target.getAttribute("data-option");
+        let computerChoice = getComputerChoice(); //get user and robot selection
+
+        toggleChoices(computerChoice, playerChoice, count);
+        playRound(playerChoice, computerChoice); //play single round
+
+
+        let getWinnerNote = document.querySelector('.winner_note');
+        let score = document.querySelector('.total_score');
+        let total_score = getScore(score, getWinnerNote.textContent); //get total score
+        
+        document.querySelector('.round').textContent = `Round: ${count + 1}`; //update round
+        
+        
+        if (count == 4){ 
+            getWinner(total_score[0], total_score[1]); //display game over
+            let hide_score = document.querySelector('.score');
+            hide_score.hidden = true;
+            let button = document.querySelector('button');
+            button.removeAttribute("hidden"); //display play again button
+            choices.disabled = true;
+            count = 0;
+            let hovers = Array.from(document.getElementsByClassName('hover'));
+            console.log(hovers);
+            for (const hover of hovers){
+                hover.setAttribute("class", ""); //disable hover
+            }
+            button.addEventListener("click", playAgain);
+            return;
+        }
+        count += 1;
+        
+    });
 
 }
+
+
+function getScore(score, getWinner){
+    //get current score
+    let player_count = Number(score.textContent[0]);
+    let computer_count = Number(score.textContent.slice(-1));
+
+
+    if (getWinner.includes("win")){
+        player_count += 1;
+    }
+    else if (getWinner.includes("lose")){
+        computer_count += 1; 
+    }
+    score.textContent = `${player_count} - ${computer_count}`;
+    return [player_count, computer_count];
+
+}
+
+function getWinner(player_count, computer_count){
+    //updates game over text
+    let gameOverNote = document.querySelector('.game_over');
+    let gameOver = null;
+
+    if (player_count == computer_count && player_count > 0){
+        gameOver = `Game over! It's a tie ${player_count} - ${computer_count}.`;
+    }
+    else if (player_count < computer_count){
+        gameOver = `Game over! You lose ${player_count} - ${computer_count}.`;
+    }
+    else{
+        gameOver = `Game over! You win ${player_count} - ${computer_count}.`;
+    }
+
+    gameOverNote.textContent = gameOver;
+    return gameOver;
+
+
+}
+
+
+playGame();
